b3adbd65e876bbb29c5a235da265763c08103df2
chenyingjing <dcba63@gmail.com>
2015-02-23 11:03:28 +0800

    Add more sample

--- CHAP08/DrawSth1/DrawSth1/t3dconsole2.cpp
+++ CHAP08/DrawSth1/DrawSth1/t3dconsole2.cpp
@@ -21,8 +21,8 @@
 // INCLUDES ///////////////////////////////////////////////
 
 #define INITGUID       // make sure all the COM interfaces are available
-                       // instead of this you can include the .LIB file
-                       // DXGUID.LIB
+// instead of this you can include the .LIB file
+// DXGUID.LIB
 
 #define WIN32_LEAN_AND_MEAN  
 
@@ -66,11 +66,11 @@
 
 //#define WINDOW_BPP        16    // bitdepth of window (8,16,24 etc.)
 #define WINDOW_BPP        8    // bitdepth of window (8,16,24 etc.)
-                                // note: if windowed and not
-                                // fullscreen then bitdepth must
-                                // be same as system bitdepth
-                                // also if 8-bit the a pallete
-                                // is created and attached
+// note: if windowed and not
+// fullscreen then bitdepth must
+// be same as system bitdepth
+// also if 8-bit the a pallete
+// is created and attached
 
 #define WINDOWED_APP      1     // 0 not windowed, 1 windowed
 
@@ -83,6 +83,7 @@ int Game_Main(void *parms=NULL);
 
 void ShowColorInPalette(UCHAR * lpSurface, LONG lPitch);
 void DrawPolygon(UCHAR * lpSurface, LONG lPitch);
+float GetScaleXY();
 
 // GLOBALS ////////////////////////////////////////////////
 
@@ -95,159 +96,159 @@ char buffer[256];                          // used to print text
 // FUNCTIONS //////////////////////////////////////////////
 
 LRESULT CALLBACK WindowProc(HWND hwnd, 
-						    UINT msg, 
-                            WPARAM wparam, 
-                            LPARAM lparam)
+							UINT msg, 
+							WPARAM wparam, 
+							LPARAM lparam)
 {
-// this is the main message handler of the system
-PAINTSTRUCT	ps;		   // used in WM_PAINT
-HDC			hdc;	   // handle to a device context
+	// this is the main message handler of the system
+	PAINTSTRUCT	ps;		   // used in WM_PAINT
+	HDC			hdc;	   // handle to a device context
 
-// what is the message 
-switch(msg)
+	// what is the message 
+	switch(msg)
 	{	
 	case WM_CREATE: 
-        {
-		// do initialization stuff here
-		return(0);
+		{
+			// do initialization stuff here
+			return(0);
 		} break;
 
-    case WM_PAINT:
-         {
-         // start painting
-         hdc = BeginPaint(hwnd,&ps);
+	case WM_PAINT:
+		{
+			// start painting
+			hdc = BeginPaint(hwnd,&ps);
 
-         // end painting
-         EndPaint(hwnd,&ps);
-         return(0);
-        } break;
+			// end painting
+			EndPaint(hwnd,&ps);
+			return(0);
+		} break;
 
 	case WM_DESTROY: 
 		{
-		// kill the application			
-		PostQuitMessage(0);
-		return(0);
+			// kill the application			
+			PostQuitMessage(0);
+			return(0);
 		} break;
 
 	default:break;
 
-    } // end switch
+	} // end switch
 
-// process any messages that we didn't take care of 
-return (DefWindowProc(hwnd, msg, wparam, lparam));
+	// process any messages that we didn't take care of 
+	return (DefWindowProc(hwnd, msg, wparam, lparam));
 
 } // end WinProc
 
 // WINMAIN ////////////////////////////////////////////////
 
 int WINAPI WinMain(	HINSTANCE hinstance,
-					HINSTANCE hprevinstance,
-					LPSTR lpcmdline,
-					int ncmdshow)
+				   HINSTANCE hprevinstance,
+				   LPSTR lpcmdline,
+				   int ncmdshow)
 {
-// this is the winmain function
-WNDCLASSEX winclass; // this will hold the class we create
-HWND	   hwnd;	 // generic window handle
-MSG		   msg;		 // generic message
-HDC        hdc;      // graphics device context
+	// this is the winmain function
+	WNDCLASSEX winclass; // this will hold the class we create
+	HWND	   hwnd;	 // generic window handle
+	MSG		   msg;		 // generic message
+	HDC        hdc;      // graphics device context
 
-// first fill in the window class stucture
-winclass.cbSize         = sizeof(WNDCLASSEX);
-winclass.style			= CS_DBLCLKS | CS_OWNDC | 
-                          CS_HREDRAW | CS_VREDRAW;
-winclass.lpfnWndProc	= WindowProc;
-winclass.cbClsExtra		= 0;
-winclass.cbWndExtra		= 0;
-winclass.hInstance		= hinstance;
-winclass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
-winclass.hCursor		= LoadCursor(NULL, IDC_ARROW); 
-winclass.hbrBackground	= (HBRUSH)GetStockObject(BLACK_BRUSH);
-winclass.lpszMenuName	= NULL;
-winclass.lpszClassName	= WINDOW_CLASS_NAME;
-winclass.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
+	// first fill in the window class stucture
+	winclass.cbSize         = sizeof(WNDCLASSEX);
+	winclass.style			= CS_DBLCLKS | CS_OWNDC | 
+		CS_HREDRAW | CS_VREDRAW;
+	winclass.lpfnWndProc	= WindowProc;
+	winclass.cbClsExtra		= 0;
+	winclass.cbWndExtra		= 0;
+	winclass.hInstance		= hinstance;
+	winclass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
+	winclass.hCursor		= LoadCursor(NULL, IDC_ARROW); 
+	winclass.hbrBackground	= (HBRUSH)GetStockObject(BLACK_BRUSH);
+	winclass.lpszMenuName	= NULL;
+	winclass.lpszClassName	= WINDOW_CLASS_NAME;
+	winclass.hIconSm        = LoadIcon(NULL, IDI_APPLICATION);
 
-// register the window class
-if (!RegisterClassEx(&winclass))
-	return(0);
+	// register the window class
+	if (!RegisterClassEx(&winclass))
+		return(0);
 
-// create the window
-if (!(hwnd = CreateWindowEx(NULL,              // extended style
-                            WINDOW_CLASS_NAME, // class
-						    WINDOW_TITLE, // title
-						    (WINDOWED_APP ? (WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION) : (WS_POPUP | WS_VISIBLE)), 
-					 	    0,0,	  // initial x,y
-						    WINDOW_WIDTH,WINDOW_HEIGHT,  // initial width, height
-						    NULL,	  // handle to parent 
-						    NULL,	  // handle to menu
-						    hinstance,// instance of this application
-						    NULL)))   // extra creation parms
-return(0);
+	// create the window
+	if (!(hwnd = CreateWindowEx(NULL,              // extended style
+		WINDOW_CLASS_NAME, // class
+		WINDOW_TITLE, // title
+		(WINDOWED_APP ? (WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION) : (WS_POPUP | WS_VISIBLE)), 
+		0,0,	  // initial x,y
+		WINDOW_WIDTH,WINDOW_HEIGHT,  // initial width, height
+		NULL,	  // handle to parent 
+		NULL,	  // handle to menu
+		hinstance,// instance of this application
+		NULL)))   // extra creation parms
+		return(0);
 
-// save the window handle and instance in a global
-main_window_handle = hwnd;
-main_instance      = hinstance;
+	// save the window handle and instance in a global
+	main_window_handle = hwnd;
+	main_instance      = hinstance;
 
-// resize the window so that client is really width x height
-if (WINDOWED_APP)
-{
-// now resize the window, so the client area is the actual size requested
-// since there may be borders and controls if this is going to be a windowed app
-// if the app is not windowed then it won't matter
-RECT window_rect = {0,0,WINDOW_WIDTH-1,WINDOW_HEIGHT-1};
-
-
-// make the call to adjust window_rect
-AdjustWindowRectEx(&window_rect,
-     GetWindowStyle(main_window_handle),
-     GetMenu(main_window_handle) != NULL,
-     GetWindowExStyle(main_window_handle));
-
-// save the global client offsets, they are needed in DDraw_Flip()
-window_client_x0 = -window_rect.left;
-window_client_y0 = -window_rect.top;
-
-// now resize the window with a call to MoveWindow()
-MoveWindow(main_window_handle,
-           0, // x position
-           0, // y position
-           window_rect.right - window_rect.left, // width
-           window_rect.bottom - window_rect.top, // height
-           FALSE);
-
-// show the window, so there's no garbage on first render
-ShowWindow(main_window_handle, SW_SHOW);
-} // end if windowed
-
-// perform all game console specific initialization
-Game_Init();
-
-// enter main event loop
-while(1)
+	// resize the window so that client is really width x height
+	if (WINDOWED_APP)
 	{
-	if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
-		{ 
-		// test if this is a quit
-        if (msg.message == WM_QUIT)
-           break;
-	
-		// translate any accelerator keys
-		TranslateMessage(&msg);
+		// now resize the window, so the client area is the actual size requested
+		// since there may be borders and controls if this is going to be a windowed app
+		// if the app is not windowed then it won't matter
+		RECT window_rect = {0,0,WINDOW_WIDTH-1,WINDOW_HEIGHT-1};
 
-		// send the message to the window proc
-		DispatchMessage(&msg);
+
+		// make the call to adjust window_rect
+		AdjustWindowRectEx(&window_rect,
+			GetWindowStyle(main_window_handle),
+			GetMenu(main_window_handle) != NULL,
+			GetWindowExStyle(main_window_handle));
+
+		// save the global client offsets, they are needed in DDraw_Flip()
+		window_client_x0 = -window_rect.left;
+		window_client_y0 = -window_rect.top;
+
+		// now resize the window with a call to MoveWindow()
+		MoveWindow(main_window_handle,
+			0, // x position
+			0, // y position
+			window_rect.right - window_rect.left, // width
+			window_rect.bottom - window_rect.top, // height
+			FALSE);
+
+		// show the window, so there's no garbage on first render
+		ShowWindow(main_window_handle, SW_SHOW);
+	} // end if windowed
+
+	// perform all game console specific initialization
+	Game_Init();
+
+	// enter main event loop
+	while(1)
+	{
+		if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+		{ 
+			// test if this is a quit
+			if (msg.message == WM_QUIT)
+				break;
+
+			// translate any accelerator keys
+			TranslateMessage(&msg);
+
+			// send the message to the window proc
+			DispatchMessage(&msg);
 		} // end if
-    
-    // main game processing goes here
-    Game_Main();
+
+		// main game processing goes here
+		Game_Main();
 
 	} // end while
 
-// shutdown game and release all resources
-Game_Shutdown();
+	// shutdown game and release all resources
+	Game_Shutdown();
 
 
-// return to Windows like this
-return(msg.wParam);
+	// return to Windows like this
+	return(msg.wParam);
 
 } // end WinMain
 
@@ -255,37 +256,40 @@ return(msg.wParam);
 
 int Game_Init(void *parms)
 {
-// this function is where you do all the initialization 
-// for your game
+	// this function is where you do all the initialization 
+	// for your game
+	Open_Error_File("errors.log");
+	Write_Error("hello, %s", "game");
+	Close_Error_File();
 
-// start up DirectDraw (replace the parms as you desire)
-DDraw_Init(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_BPP, WINDOWED_APP);
+	// start up DirectDraw (replace the parms as you desire)
+	DDraw_Init(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_BPP, WINDOWED_APP);
 
-// initialize directinput
-DInput_Init();
+	// initialize directinput
+	DInput_Init();
 
-// acquire the keyboard 
-DInput_Init_Keyboard();
+	// acquire the keyboard 
+	DInput_Init_Keyboard();
 
-// add calls to acquire other directinput devices here...
+	// add calls to acquire other directinput devices here...
 
 
-// initialize directsound and directmusic
-DSound_Init();
-DMusic_Init();
+	// initialize directsound and directmusic
+	DSound_Init();
+	DMusic_Init();
 
-// hide the mouse
-if (!WINDOWED_APP)
-    ShowCursor(FALSE);
+	// hide the mouse
+	if (!WINDOWED_APP)
+		ShowCursor(FALSE);
 
-// seed random number generator
-srand(Start_Clock());
+	// seed random number generator
+	srand(Start_Clock());
 
-// all your initialization code goes here...
-Build_Sin_Cos_Tables();
+	// all your initialization code goes here...
+	Build_Sin_Cos_Tables();
 
-// return success
-return(1);
+	// return success
+	return(1);
 
 } // end Game_Init
 
@@ -293,93 +297,103 @@ return(1);
 
 int Game_Shutdown(void *parms)
 {
-// this function is where you shutdown your game and
-// release all resources that you allocated
+	// this function is where you shutdown your game and
+	// release all resources that you allocated
 
-// shut everything down
+	// shut everything down
 
-// release all your resources created for the game here....
+	// release all your resources created for the game here....
 
 
-// now directsound
-DSound_Stop_All_Sounds();
-DSound_Delete_All_Sounds();
-DSound_Shutdown();
+	// now directsound
+	DSound_Stop_All_Sounds();
+	DSound_Delete_All_Sounds();
+	DSound_Shutdown();
 
-// directmusic
-DMusic_Delete_All_MIDI();
-DMusic_Shutdown();
+	// directmusic
+	DMusic_Delete_All_MIDI();
+	DMusic_Shutdown();
 
-// shut down directinput
-DInput_Release_Keyboard();
+	// shut down directinput
+	DInput_Release_Keyboard();
 
-DInput_Shutdown();
+	DInput_Shutdown();
 
-// shutdown directdraw last
-DDraw_Shutdown();
+	// shutdown directdraw last
+	DDraw_Shutdown();
 
-// return success
-return(1);
+	// return success
+	return(1);
 } // end Game_Shutdown
 
 //////////////////////////////////////////////////////////
 
 int Game_Main(void *parms)
 {
-// this is the workhorse of your game it will be called
-// continuously in real-time this is like main() in C
-// all the calls for you game go here!
+	// this is the workhorse of your game it will be called
+	// continuously in real-time this is like main() in C
+	// all the calls for you game go here!
 
-int index; // looping var
+	int index; // looping var
 
-// start the timing clock
-Start_Clock();
+	// start the timing clock
+	Start_Clock();
 
-// clear the drawing surface 
-DDraw_Fill_Surface(lpddsback, 0);
+	// clear the drawing surface 
+	DDraw_Fill_Surface(lpddsback, 0);
 
-// read keyboard and other devices here
-DInput_Read_Keyboard();
+	// read keyboard and other devices here
+	DInput_Read_Keyboard();
 
-// game logic here...
-// lock primary buffer
-DDRAW_INIT_STRUCT(ddsd);
+	// game logic here...
+	// lock primary buffer
+	DDRAW_INIT_STRUCT(ddsd);
 
-if (FAILED(lpddsback->Lock(NULL,&ddsd,
-                           DDLOCK_WAIT | DDLOCK_SURFACEMEMORYPTR,
-                           NULL)))
-return(0);
+	if (FAILED(lpddsback->Lock(NULL,&ddsd,
+		DDLOCK_WAIT | DDLOCK_SURFACEMEMORYPTR,
+		NULL)))
+		return(0);
 
-// draw the triangle
-Draw_Triangle_2D(100, 0, 0, 100, 200, 100, 100,(UCHAR *)ddsd.lpSurface, ddsd.lPitch);
+	// draw the triangle
+	Draw_Triangle_2D(100, 0, 0, 100, 200, 100, 100,(UCHAR *)ddsd.lpSurface, ddsd.lPitch);
 
-ShowColorInPalette((UCHAR *)ddsd.lpSurface, ddsd.lPitch);
+	ShowColorInPalette((UCHAR *)ddsd.lpSurface, ddsd.lPitch);
 
-DrawPolygon((UCHAR *)ddsd.lpSurface, ddsd.lPitch);
+	DrawPolygon((UCHAR *)ddsd.lpSurface, ddsd.lPitch);
 
-Draw_Clip_Line(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 200,(UCHAR *)ddsd.lpSurface, ddsd.lPitch);
+	Draw_Clip_Line(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 200,(UCHAR *)ddsd.lpSurface, ddsd.lPitch);
 
-// unlock primary buffer
-if (FAILED(lpddsback->Unlock(NULL)))
-   return(0);
+	Draw_Pixel(200, 200, 201, (UCHAR *)ddsd.lpSurface, ddsd.lPitch);
+
+	HLine(10, 400, 300, 200, (UCHAR *)ddsd.lpSurface, ddsd.lPitch);
+
+	VLine(10, 300, 200, 200, (UCHAR *)ddsd.lpSurface, ddsd.lPitch);
 
 
-// flip the surfaces
-DDraw_Flip();
+	// unlock primary buffer
+	if (FAILED(lpddsback->Unlock(NULL)))
+		return(0);
 
-// sync to 30ish fps
-Wait_Clock(30);
+	Draw_Rectangle(200, 250, 300, 350, 200, lpddsback);
 
-// check of user is trying to exit
-if (KEY_DOWN(VK_ESCAPE) || keyboard_state[DIK_ESCAPE])
-    {
-    PostMessage(main_window_handle, WM_DESTROY,0,0);
+	// flip the surfaces
+	DDraw_Flip();
 
-    } // end if
+	Draw_Text_GDI("This is a test", 200, 300, RGB(255, 255, 255), lpddsprimary);
 
-// return success
-return(1);
- 
+	// sync to 30ish fps
+	Wait_Clock(30);
+
+	// check of user is trying to exit
+	if (KEY_DOWN(VK_ESCAPE) || keyboard_state[DIK_ESCAPE])
+	{
+		PostMessage(main_window_handle, WM_DESTROY,0,0);
+
+	} // end if
+
+	// return success
+	return(1);
+
 } // end Game_Main
 
 void ShowColorInPalette(UCHAR * lpSurface, LONG lPitch)
@@ -429,9 +443,11 @@ void DrawPolygon(UCHAR * lpSurface, LONG lPitch)
 	//Translate_Polygon2D(&fivePoly, 10, 0);
 	static int theta = 0;
 	Rotate_Polygon2D(&fivePoly, theta);
+	float scaleXY = GetScaleXY();
+	Scale_Polygon2D(&fivePoly, scaleXY, scaleXY);
 	Draw_Filled_Polygon2D(&fivePoly, lpSurface, lPitch);
 
-/////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////
 
 	fivePolyX0 += 10;
 	if (fivePolyX0 > WINDOW_WIDTH)
@@ -445,6 +461,25 @@ void DrawPolygon(UCHAR * lpSurface, LONG lPitch)
 	}
 
 	theta += 1;
+}
+
+float GetScaleXY()
+{
+	static float scaleXYArray[] = {1, 0.9, 0.8, 0.7, 0.6, 0.5};
+	static int scaleXYArrayIndex = 0;
+	static int scaleXYArrayStep = 1;
+	int scaleXYArrayLength = sizeof(scaleXYArray)/sizeof(scaleXYArray[0]);
+	float returnValue = scaleXYArray[scaleXYArrayIndex];
+	if (scaleXYArrayIndex >= scaleXYArrayLength)
+	{
+		scaleXYArrayStep = -1;
+	}
+	if (scaleXYArrayIndex < 0)
+	{
+		scaleXYArrayStep = 1;
+	}
+	scaleXYArrayIndex += scaleXYArrayStep;
+	return returnValue;
 }
 
 
